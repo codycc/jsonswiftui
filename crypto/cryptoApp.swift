437ab259cc50022ac9901b8c2cdcb1cc440ec909
Cody Condon <linkmecody@gmail.com>
2021-07-19 16:13:37 -0400

    Initial Commit

---
+++ crypto/cryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoApp.swift
+//  crypto
+//
+//  Created by Cody Condon on 2021-07-19.
+//
+
+import SwiftUI
+
+@main
+struct cryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
